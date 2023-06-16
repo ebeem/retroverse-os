898a8d9ba4a8cd40bcc630a75979affbf6d2c7c4
Almarhoon Ibraheem <ibeem@outlook.com>
2023-06-16 06:51:58 +0300

    fix bluetooth script and auto pairing

--- packages/sx05re/emuelec/config/custom_start.sh
+++ packages/sx05re/emuelec/config/custom_start.sh
@@ -9,39 +9,42 @@
 # Place any scripts you need to run at boot on this file
 
 case "${1}" in
-"before")
+  "before")
 
-# Any commands that you want to run before the frontend begins should go here
+    # run auto bluetooth pairing in background
+    emuelec-bluetooth 10 -1 >>~/.config/emuelec/logs/bluetooth.log &
 
-# example BT config, use only as a last resort
-# Bluetooth, Make sure you change your BT MAC address, you need to do this by SSH the first time
-# by running 
+    # Any commands that you want to run before the frontend begins should go here
 
-# hcitool scan
-# bluetoothctl pair yourmac
-# bluetoothctl trust yourmac 
+    # example BT config, use only as a last resort
+    # Bluetooth, Make sure you change your BT MAC address, you need to do this by SSH the first time
+    # by running
 
-# If you want to use bluetooth, uncomment every line after this one 
+    # hcitool scan
+    # bluetoothctl pair yourmac
+    # bluetoothctl trust yourmac
 
-# BTMAC="E4:17:D8:8B:F1:80"
-# (
-# echo "agent on" | bluetoothctl
-# echo "default-agent" | bluetoothctl
-# echo "power on" | bluetoothctl
-# echo "discoverable on" | bluetoothctl
-# echo "pairable on" | bluetoothctl
-# echo "scan on" | bluetoothctl
-# echo "trust $BTMAC" | bluetoothctl
-# echo "connect $BTMAC" | bluetoothctl
-# )&
+    # If you want to use bluetooth, uncomment every line after this one
 
-	exit 0
-	;;
-*)
-# Any commands that you want to run after the frontend has started goes here
+    # BTMAC="E4:17:D8:8B:F1:80"
+    # (
+    # echo "agent on" | bluetoothctl
+    # echo "default-agent" | bluetoothctl
+    # echo "power on" | bluetoothctl
+    # echo "discoverable on" | bluetoothctl
+    # echo "pairable on" | bluetoothctl
+    # echo "scan on" | bluetoothctl
+    # echo "trust $BTMAC" | bluetoothctl
+    # echo "connect $BTMAC" | bluetoothctl
+    # )&
 
     exit 0
-	;;
+    ;;
+  *)
+    # Any commands that you want to run after the frontend has started goes here
+
+    exit 0
+    ;;
 esac
 ## nothing was called so exit
 exit 0

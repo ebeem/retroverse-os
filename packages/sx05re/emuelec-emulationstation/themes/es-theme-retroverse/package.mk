957de5fe33b3684fea92ddc11f979101262679b4
Almarhoon Ibraheem <ibeem@outlook.com>
2023-07-01 20:59:06 +0300

    add retroverse theme

---
+++ packages/sx05re/emuelec-emulationstation/themes/es-theme-retroverse/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
+
+PKG_NAME="es-theme-retroverse"
+PKG_VERSION="950dcb405104fc70b425ddcd1d7da3af64fe6cbb"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/ebeem/es-theme-retroverse"
+PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emuelec"
+PKG_SHORTDESC="The EmulationStation theme Retroverse based on epic-noir"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  : not
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/config/emulationstation/themes/es-theme-retroverse
+    cp -r * $INSTALL/usr/config/emulationstation/themes/es-theme-retroverse
+}

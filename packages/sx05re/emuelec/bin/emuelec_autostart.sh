fa42e28250b190efca1cd8a34bd24cb0d933dfe6
Almarhoon Ibraheem <ibeem@outlook.com>
2023-07-03 23:02:31 +0300

    fix bluetooth pairing script

--- packages/sx05re/emuelec/bin/emuelec_autostart.sh
+++ packages/sx05re/emuelec/bin/emuelec_autostart.sh
@@ -6,74 +6,72 @@
 # Source predefined functions and variables
 . /etc/profile
 
-# DO NOT modify this file, if you need to use autostart please use /storage/.config/custom_start.sh 
+# DO NOT modify this file, if you need to use autostart please use /storage/.config/custom_start.sh
 
 # It seems some slow SDcards have a problem creating the symlink on time :/
 CONFIG_DIR="/storage/.emulationstation"
 CONFIG_DIR2="/storage/.config/emulationstation"
 
 if [ ! -L "$CONFIG_DIR" ]; then
-ln -sf $CONFIG_DIR2 $CONFIG_DIR
+  ln -sf $CONFIG_DIR2 $CONFIG_DIR
 fi
 
 if [ "${EE_DEVICE}" == "Amlogic" ]; then
-  rm /storage/.config/asound.conf > /dev/null 2>&1
+  rm /storage/.config/asound.conf >/dev/null 2>&1
   cp /storage/.config/asound.conf-amlogic /storage/.config/asound.conf
 
-    if [ "$(get_es_setting bool StopMusicOnScreenSaver)" != "false" ]; then 
-        sed -i "/<bool name=\"StopMusicOnScreenSaver.*/d" "${ES_CONF}"
-        sed -i "s|</config>|	<bool name=\"StopMusicOnScreenSaver\" value=\"false\" />\n</config>|g" "${ES_CONF}"
-    fi
+  if [ "$(get_es_setting bool StopMusicOnScreenSaver)" != "false" ]; then
+    sed -i "/<bool name=\"StopMusicOnScreenSaver.*/d" "${ES_CONF}"
+    sed -i "s|</config>|	<bool name=\"StopMusicOnScreenSaver\" value=\"false\" />\n</config>|g" "${ES_CONF}"
+  fi
 
 elif [ "${EE_DEVICE}" == "Amlogic-ng" ]; then
-  rm /storage/.config/asound.conf > /dev/null 2>&1
+  rm /storage/.config/asound.conf >/dev/null 2>&1
   cp /storage/.config/asound.conf-amlogic-ng /storage/.config/asound.conf
 fi
 
 HOSTNAME=$(get_ee_setting system.hostname)
-if [ ! -z "${HOSTNAME}" ];then 
-    echo "${HOSTNAME}" > /storage/.cache/hostname
+if [ ! -z "${HOSTNAME}" ]; then
+  echo "${HOSTNAME}" >/storage/.cache/hostname
 else
-    echo "EMUELEC" > /storage/.cache/hostname
+  echo "EMUELEC" >/storage/.cache/hostname
 fi
-cat /storage/.cache/hostname > /proc/sys/kernel/hostname
+cat /storage/.cache/hostname >/proc/sys/kernel/hostname
 
 if [[ "$EE_DEVICE" == "GameForce" ]]; then
-LED=$(get_ee_setting bl_rgb)
-[ -z "${LED}" ] && LED="Off"
-odroidgoa_utils.sh bl "${LED}"
+  LED=$(get_ee_setting bl_rgb)
+  [ -z "${LED}" ] && LED="Off"
+  odroidgoa_utils.sh bl "${LED}"
 
-LED=$(get_ee_setting gf_statusled)
-[ -z "${LED}" ] && LED="heartbeat"
-odroidgoa_utils.sh pl "${LED}"
+  LED=$(get_ee_setting gf_statusled)
+  [ -z "${LED}" ] && LED="heartbeat"
+  odroidgoa_utils.sh pl "${LED}"
 
-
-rk_wifi_init /dev/ttyS1
+  rk_wifi_init /dev/ttyS1
 fi
 
 if [[ "$EE_DEVICE" == "GameForce" ]] || [[ "$EE_DEVICE" == "OdroidGoAdvance" ]]; then
-    if [ -e "/flash/no_oc.oga" ]; then 
-        set_ee_setting ee_oga_oc disable
-        OGAOC=""
-    else
-        OGAOC=$(get_ee_setting ee_oga_oc)
-    fi
-[ -z "${OGAOC}" ] && OGAOC="Off"
-    odroidgoa_utils.sh oga_oc "${OGAOC}"
+  if [ -e "/flash/no_oc.oga" ]; then
+    set_ee_setting ee_oga_oc disable
+    OGAOC=""
+  else
+    OGAOC=$(get_ee_setting ee_oga_oc)
+  fi
+  [ -z "${OGAOC}" ] && OGAOC="Off"
+  odroidgoa_utils.sh oga_oc "${OGAOC}"
 fi
-
 
 # Mounts /storage/roms
 MOUNT_HANDLER=$(get_ee_setting ee_mount.handler)
 if [ -z "$MOUNT_HANDLER" ]; then
   MOUNT_HANDLER="eemount"
 fi
-$MOUNT_HANDLER &> /emuelec/logs/eemount.log
+$MOUNT_HANDLER &>/emuelec/logs/eemount.log
 
 # copy default bezel to /storage/roms/bezel if it doesn't exists
-if [ ! -f "/storage/roms/bezels/default.cfg" ]; then 
-mkdir -p /storage/roms/bezels/
-cp -rf /usr/share/retroarch-overlays/bezels/* /storage/roms/bezels/ &
+if [ ! -f "/storage/roms/bezels/default.cfg" ]; then
+  mkdir -p /storage/roms/bezels/
+  cp -rf /usr/share/retroarch-overlays/bezels/* /storage/roms/bezels/ &
 fi
 
 # Restore config if backup exists
@@ -82,8 +80,8 @@ BACKUPFILE="/storage/roms/backup/${BACKUPTAR}"
 
 [[ ! -f "${BACKUPFILE}" ]] && BACKUPFILE="/var/media/EEROMS/backup/${BACKUPTAR}"
 
-if [ -f "${BACKUPFILE}" ]; then 
-	emuelec-utils ee_backup restore no > /emuelec/logs/last-restore.log 2>&1
+if [ -f "${BACKUPFILE}" ]; then
+  emuelec-utils ee_backup restore no >/emuelec/logs/last-restore.log 2>&1
 fi
 
 # Clean cache garbage when boot up.
@@ -93,21 +91,21 @@ rm -rf /storage/.cache/cores/* &
 DEFE=$(get_ee_setting ee_ssh.enabled)
 
 case "$DEFE" in
-"0")
-	systemctl stop sshd
-	rm /storage/.cache/services/sshd.conf
-	;;
-*)
-	mkdir -p /storage/.cache/services/
-	touch /storage/.cache/services/sshd.conf
-	systemctl start sshd
-	;;
+  "0")
+    systemctl stop sshd
+    rm /storage/.cache/services/sshd.conf
+    ;;
+  *)
+    mkdir -p /storage/.cache/services/
+    touch /storage/.cache/services/sshd.conf
+    systemctl start sshd
+    ;;
 esac
 
 # Checks and sets the resolution for starting ES.
 check_res.sh
 
-# Show splash creen 
+# Show splash creen
 show_splash.sh intro
 
 # run custom_start before FE scripts
@@ -120,28 +118,28 @@ wait
 BTENABLED=$(get_ee_setting ee_bluetooth.enabled)
 
 if [[ "$BTENABLED" != "1" ]]; then
-systemctl stop bluetooth
-rm /storage/.cache/services/bluez.conf & 
+  systemctl stop bluetooth
+  rm /storage/.cache/services/bluez.conf &
 else
-systemctl restart bluetooth
-# run auto bluetooth pairing in background
-emuelec-bluetooth -1 >>~/.config/emuelec/logs/bluetooth.log &
+  systemctl restart bluetooth
+  # run auto bluetooth pairing in background
+  emuelec-bluetooth -1 >>~/.config/emuelec/logs/bluetooth.log &
 fi
 
 # What to start at boot?
 DEFE=$(get_ee_setting ee_boot)
 
 case "$DEFE" in
-"Retroarch")
-	rm -rf /var/lock/start.retro
-	touch /var/lock/start.retro
-	systemctl start retroarch
-	;;
-*)
-	rm /var/lock/start.games
-	touch /var/lock/start.games
+  "Retroarch")
+    rm -rf /var/lock/start.retro
+    touch /var/lock/start.retro
+    systemctl start retroarch
+    ;;
+  *)
+    rm /var/lock/start.games
+    touch /var/lock/start.games
     systemctl start emustation
-	;;
+    ;;
 esac
 
 # run custom_start ending scripts
--- packages/sx05re/emuelec/config/custom_start.sh
+++ packages/sx05re/emuelec/config/custom_start.sh
@@ -9,39 +9,39 @@
 # Place any scripts you need to run at boot on this file
 
 case "${1}" in
-  "before")
+"before")
 
-    # Any commands that you want to run before the frontend begins should go here
+# Any commands that you want to run before the frontend begins should go here
 
-    # example BT config, use only as a last resort
-    # Bluetooth, Make sure you change your BT MAC address, you need to do this by SSH the first time
-    # by running
+# example BT config, use only as a last resort
+# Bluetooth, Make sure you change your BT MAC address, you need to do this by SSH the first time
+# by running 
 
-    # hcitool scan
-    # bluetoothctl pair yourmac
-    # bluetoothctl trust yourmac
+# hcitool scan
+# bluetoothctl pair yourmac
+# bluetoothctl trust yourmac 
 
-    # If you want to use bluetooth, uncomment every line after this one
+# If you want to use bluetooth, uncomment every line after this one 
 
-    # BTMAC="E4:17:D8:8B:F1:80"
-    # (
-    # echo "agent on" | bluetoothctl
-    # echo "default-agent" | bluetoothctl
-    # echo "power on" | bluetoothctl
-    # echo "discoverable on" | bluetoothctl
-    # echo "pairable on" | bluetoothctl
-    # echo "scan on" | bluetoothctl
-    # echo "trust $BTMAC" | bluetoothctl
-    # echo "connect $BTMAC" | bluetoothctl
-    # )&
+# BTMAC="E4:17:D8:8B:F1:80"
+# (
+# echo "agent on" | bluetoothctl
+# echo "default-agent" | bluetoothctl
+# echo "power on" | bluetoothctl
+# echo "discoverable on" | bluetoothctl
+# echo "pairable on" | bluetoothctl
+# echo "scan on" | bluetoothctl
+# echo "trust $BTMAC" | bluetoothctl
+# echo "connect $BTMAC" | bluetoothctl
+# )&
+
+	exit 0
+	;;
+*)
+# Any commands that you want to run after the frontend has started goes here
 
     exit 0
-    ;;
-  *)
-    # Any commands that you want to run after the frontend has started goes here
-
-    exit 0
-    ;;
+	;;
 esac
 ## nothing was called so exit
 exit 0
